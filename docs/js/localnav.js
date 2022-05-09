bff01637763bc1b647e0f8c54bf7466ced97f42b
wraydev <79096046+wraydev@users.noreply.github.com>
2022-05-09 18:05:52 +0100

    reformatted lab template

--- docs/js/localnav.js
+++ docs/js/localnav.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
     $("h2").each(function() {
       var text = $(this).text()
       var id = $(this).attr("id")
-      var navLink = "<li><a href='#" + id + "'>" + text + "</a></li>"
+      var navLink = "<li id='ln" + id + "'><a href='#" + id + "'>" + text + "</a></li>"
       $("#localnav").append(navLink)
     }) 
     
-  })
+  });
+  
