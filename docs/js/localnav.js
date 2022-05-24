0b59e096ea607d5b788392893a16f931ea9b636c
wraydev <79096046+wraydev@users.noreply.github.com>
2022-05-24 12:34:33 +0100

    Large update to site format included autogeneration of sub-menu for navigation

--- docs/js/localnav.js
+++ docs/js/localnav.js
@@ -1,11 +1,16 @@
-$(document).ready(function() {
-  
-    $("h2").each(function() {
-      var text = $(this).text()
-      var id = $(this).attr("id")
-      var navLink = "<li id='ln" + id + "'><a href='#" + id + "'>" + text + "</a></li>"
-      $("#localnav").append(navLink)
-    }) 
-    
+
+$(document).ready(function () {
+
+  /* Add the sublist to the active naveigation list item*/
+  var localNavAtt = '<ul title="Page Contents" class="localnav"></ul>';
+  $(".activeListItem").append(localNavAtt);
+
+  /* Add the link to each h2 to the sub menu*/
+  $("h2").each(function () {
+    var text = $(this).text();
+    var id = $(this).attr("id");
+    var navLink = "<li id='ln" + id + "'><a href='#" + id + "'>" + text + "</a></li>";
+    $(".localnav").append(navLink);
   });
-  
+
+});
